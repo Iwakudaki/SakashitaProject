3d99b812b0555787a943ab6cff5d4010fae68422
Iwakudaki <workpc115@outlook.jp>
2019-06-08 02:42:33 +0900

    Playerスクリプト　リファクタリング Gunスクリプト　追加

--- VRzemi/Assets/Scripts/Main/Player/Player.cs
+++ VRzemi/Assets/Scripts/Main/Player/Player.cs
@@ -5,43 +5,31 @@ using UnityEngine.Assertions;
 
 public class Player : Character {
 	[ SerializeField ] private GazeController _gaze_controller = null;
-	[ SerializeField ] private GameObject _bullet = null;
+	[ SerializeField ] private Gun _gun = null;
 	[ SerializeField ] private float _pos_z = 0;
 	[ SerializeField ] private float _spped = 0;
 	[ SerializeField ] private float _move_limit = 0;
 	[ SerializeField ] private float _direction_size = 0;
-	[ SerializeField ] private float _set_rate_speed = 0;	//射撃のレート
 
 	private Controller _controller = new Controller( );
-	private GameObject _lock_on_obj = null;
 	private Vector3 _limit_pos = new Vector3( 0, 0, 0 );
 	private float _pre_pos_x = 0;
-	private float _rate_speed = 0;
-	private bool _is_shoot = false;
 
 	private void Start( ) {
 		Assert.IsNotNull( _gaze_controller, "[Player]GazeControllerの参照がありません" );
 
 		_limit_pos = new Vector3( transform.position.x + _move_limit, 0, _pos_z );
 		transform.position = new Vector3( 0, 0, _pos_z );
-		_rate_speed = _set_rate_speed;
 	}
 
 	private void FixedUpdate( ) {
 		Move( );
-		NextShootCount( );
 	}
 
 	private void Update( ) {
 		Direction( );
 
 		if ( _controller.getCotrollerInput( Controller.INPUT_TYPE.TRIGGER ) ) {
-			_is_shoot = true;
-		} else { 
-			_is_shoot = false;	
-		}
-
-		if ( _is_shoot ) { 
 			Shoot( );
 		}
 
@@ -69,24 +57,10 @@ public class Player : Character {
 	}
 
 	private void Shoot( ) {
-		//敵をロックオンしていなかったら撃たない
-		if ( _gaze_controller.getHitObject( ) == null ) {
-			_lock_on_obj = null;
-			return;
+		//敵をロックオンしていたら撃つ
+		if ( _gaze_controller.getHitObject( ) != null ) {
+			_gun.Shoot( _gaze_controller.getHitObject( ) );	
 		}
-
-		if ( !getIsShoot( ) ) return;
-
-		//if ( _lock_on_obj == _gaze_controller.getHitObject( ) ) return;
-
-		_lock_on_obj = _gaze_controller.getHitObject( );
-		Vector3 bullet_dir = _lock_on_obj.transform.position - transform.position;
-		GameObject bullet_obj = Instantiate( _bullet, transform.position, Quaternion.LookRotation( bullet_dir.normalized ) );
-		_rate_speed = _set_rate_speed;	//レートリセット
-
-		//どうにかしないと
-		Bullet bullet = bullet_obj.GetComponent< Bullet >( );
-		bullet.setTarget( _lock_on_obj );
 	}
 
 	//Playerが動ける範囲
@@ -105,21 +79,6 @@ public class Player : Character {
 	private void Direction( ) {
 		Vector3 player_dir = new Vector3( -_gaze_controller.getDirection( ).y, _gaze_controller.getDirection( ).x, 0 );	//恐らく、2Dと3Dで向きの計算が違うため入れ替えてる(2Dは単純にｘは横、ｙは縦。3Dはそれぞれの線の軸を中心に回転する)
 		transform.eulerAngles = player_dir * _direction_size;
-	}
-
-	private void NextShootCount( ) { 
-		if ( _is_shoot ) {
-			_rate_speed -= Time.deltaTime;
-			if ( _rate_speed < 0 ) { 
-				_rate_speed = 0;	
-			}
-		} else {
-			_rate_speed = _set_rate_speed;	
-		}
-	}
-
-	private bool getIsShoot( ) {
-		return _rate_speed <= 0;
 	}
 
 }
