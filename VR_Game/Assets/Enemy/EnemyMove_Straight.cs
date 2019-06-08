948ef666d930d4108a36acd5d83a738c24e32dff
mamizosan <ymjftokinotabibito@gmail.com>
2019-06-08 23:32:57 +0900

    aaa

---
+++ VR_Game/Assets/Enemy/EnemyMove_Straight.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove_Straight : MonoBehaviour
+{
+    public float speed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 pos = this.transform.position;
+        this.transform.position = new Vector3(0, 0, pos.z - speed);
+
+    }
+}
