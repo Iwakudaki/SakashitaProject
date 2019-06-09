6b001909b50f91102fd052e3ea2305f81703c22b
mamizosan <ymjftokinotabibito@gmail.com>
2019-06-09 18:09:39 +0900

    スクリプトArcMove完成

--- VRzemi/Assets/Scripts/Main/Enemy/ArcMove.cs
+++ VRzemi/Assets/Scripts/Main/Enemy/ArcMove.cs
@@ -7,9 +7,14 @@ public class ArcMove : EnemyMove {
     public float radius = 1.0f;
     public float speed = 1.0f;
 
+    private float pos_x;
+    private float pos_z;
+
     public Vector3 Move(float speed) {
 
-            return new Vector3(-1 , 0, -1) * speed;
+        Arc();
+
+            return new Vector3(pos_x - speed, 0, pos_z - speed);
     }
 
       public void Arc() {
@@ -17,13 +22,12 @@ public class ArcMove : EnemyMove {
         
 
 
-        if (pos.z != 0 && pos.x != -1)
+        if (pos_z != 0 && pos_x != -1)
         {
 
-            pos.x = radius * Mathf.Sin(Time.time * speed);
-            pos.z = radius * Mathf.Cos(Time.time * speed);
+            pos_x = radius * Mathf.Sin(Time.time * speed);
+            pos_z = radius * Mathf.Cos(Time.time * speed);
 
-           transform.position = new Vector3(pos.x - speed, 0, pos.z - speed);
         }
     }
 
